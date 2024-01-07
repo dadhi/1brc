6f929b2e36acb2a0f63cd1adbf9b9ce04fb21f88
dadhi <max.volkov@gmail.com>
2024-01-07 16:10:19 +0100

    working with weather stations instead

--- 1brc/Program.cs
+++ 1brc/Program.cs
@@ -2,12 +2,24 @@
 
 namespace _1brc;
 
+/*
+# Results on my machine:
+
+$ dotnet run -c Release --project ./1brc
+Total row count 44,691
+
+## No more tuples for chunks
+
+Processed in 00:00:00.1270662
+
+*/
+
 class Program
 {
     static void Main(string[] args)
     {
         var sw = Stopwatch.StartNew();
-        var path = args.Length > 0 ? args[0] : "D:/tmp/1brc_temp/1brc_1B.txt";
+        var path = args.Length > 0 ? args[0] : @"C:\oss\1brc\weather_stations.csv";
         using var app = new App(path);
         // Console.WriteLine($"Chunk count: {app.SplitIntoMemoryChunks().Count}");
         app.PrintResult();
